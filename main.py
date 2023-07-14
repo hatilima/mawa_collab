f4f75b46d28bbbed89966a71d9b7a8893e12fb59
Nyendwa <jannynyendwa@gmail.com>
2023-07-14 16:08:33 +0200

    second commit

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 class Person(object):
-	def __init__(self, age, name):
-		self.age = age
-		self.name = name
+    def __init__(self, age, name):
+        self.age = age
+        self.name = name
 
-	def show_person(self):
-		print("The person is named "+self.name+".")
-		print(self.name+" is "+str(self.age)+" years old!")
+    def show_person(self):
+        print("The person is named " + self.name + ".")
+        print(self.name + " is " + str(self.age) + " years old!")
 
 
 
 nyendwa = Person(2, "Chawanzi")
+
+nyendwa = Person(62, "Bally")
 
 nyendwa.show_person()
